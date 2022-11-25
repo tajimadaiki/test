cc78f17e3aaa6101321327fc09e5aea436b3ac80
DaikiTajima <t.dai.1215@gmail.com>
2022-11-25 11:04:14 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #/!bin/bash
+# SPDX-FileCopyrightText: 2022 Daiki Tajima
+# SPDX-License-Identifer: BSD-3-Clause
 
+ng(){
+  echo NG at Line $1
+  res=1
+}
+
+res=0
+
+### I/O test ###
 out=$(seq 10 | ./plus)
+[ "${out}" = 55 ] || ng $LINENO
 
-[ "${out}" = 55 ]
+[ "$res" = 0 ] && echo OK
+exit $res
